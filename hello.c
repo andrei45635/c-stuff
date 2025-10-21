8f8f0b2eae0934252c1b0fd812eb7ba044554c66
andrei45635 <iacob.andrei34@gmail.com>
2025-10-21 21:37:15 +0300

    /g/

--- hello.c
+++ hello.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    
-    int c = 10;
+	
+	char cities[4][10]= {"Berlin", "Amsterdam", "Cologne", "LA"};	
 
-    for(int i = 0; i < c; i++) {
-    	printf("%d", i);
-    }
+	for (int i = 0; strcmp(cities[i], "") != 0; i++) {
+		if (strlen(cities[i]) >= 5) {
+			printf("%s\n", cities[i]);
+		}	
+	}
 
-    return 0;
+	return 0;
 }
